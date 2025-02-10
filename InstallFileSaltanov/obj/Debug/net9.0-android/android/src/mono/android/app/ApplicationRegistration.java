02782c07be2158f764f6ad68d669b07dbd53e6f4
Saltanov <den050319@gmail.com>
2025-02-10 18:07:12 +0300

    Initial

---
+++ InstallFileSaltanov/obj/Debug/net9.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("InstallFileSaltanov.MainApplication, InstallFileSaltanov, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64f19568219909f0cd.MainApplication.class, crc64f19568219909f0cd.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
